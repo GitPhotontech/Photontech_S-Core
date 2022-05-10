8da4195ce280f6d18fe77b8b2b01f431603336e7
Photontech <102208603+GitPhotontech@users.noreply.github.com>
2022-05-10 13:13:43 +0300

    Delete blink.py

--- blink.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin, Timer	#Pico için pinler ve Timer kütüphaneleri dahil edildi
-led = Pin(25, Pin.OUT)		#25. pin çıkış pini olarak ayarlandı
-timer = Timer()			#Timer fonksiyonu timer değişkenine atandı
-
-def blink(timer):		#Blink uygulaması için gerekli fonksiyon yazıldı
-    led.toggle()		
-
-timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)	#timer için özel konfigürasyonlar belirlendi
