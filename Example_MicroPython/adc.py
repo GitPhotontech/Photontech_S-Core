74fefa00156734e9d5c6881829424f71a19594b8
Photontech <102208603+GitPhotontech@users.noreply.github.com>
2022-05-17 23:01:16 +0300

    Add files via upload

---
+++ Example_MicroPython/adc.py
@@ -0,0 +1,22 @@
+""" 
+PHOTONTECH 
+
+  S-Core ADC örneği
+  
+  S-Core üzerinde ki 26. pine bağlı potansiyometre çevrildikçe okunan değer print komutu ile ekrana yazdırılıyor.
+
+   
+  Bu örnek kod micropython ile yazılarak PHOTONTECH tarafından kamuya sunulmuştur.
+  
+  https://github.com/GitPhotontech/Photontech_AlphaBoard
+  
+"""
+
+from machine import ADC,Pin                     #Piconun pinleri ve adc kütphanesi tanımlandı.
+import time                                     #Bekleme oluşturmak için time kütüphanesi eklendi
+
+adc = ADC(Pin(26))                              #26.pin ADC giriş pini olarak tanımlandı.
+
+while True:                                     #Program sonsuz döngü içerisine alındı.
+    print(adc.read_u16())                       #Okunan potansiyometre değerleri ekrana basılıyor.
+    time.sleep(0.2)                             #0.2sn bekleme oluşturuluyor.
---
+++ Example_MicroPython/button.py
@@ -0,0 +1,25 @@
+""" 
+PHOTONTECH 
+
+  S-Core Buton Örneği
+  
+  S-Core üzerinde ki 14. pine bağlı butona basıldığında 25.pine bağlı led yakılıyor.
+  Butondan parmağımızı çektiğimizde led söndürülüyor.
+   
+  Bu örnek kod micropython ile yazılarak PHOTONTECH tarafından kamuya sunulmuştur.
+  
+  https://github.com/GitPhotontech/Photontech_AlphaBoard
+  
+"""
+
+from machine import Pin                         #Piconun pinleri tanımlandı.
+import time                                     #Bekleme oluşturmak için time kütüphanesi eklendi
+
+led = Pin(25, Pin.OUT)                          #25.Pin led için çıkış bacağı olarak tanımlandı.
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)         #14.Pin buton için giriş pini olarak tanımlandı.
+
+while True:                                     #Program sonsuz döngü içerisine alındı.
+    if(button.value()==True):                   #Butona basılma durumu kontrol ediliyor.
+        led.value(1)                            #Butona basıldıysa led yakılıyor.
+    else:
+        led.value(0)                            #Butona basılmadıysa led söndürülüyor.
