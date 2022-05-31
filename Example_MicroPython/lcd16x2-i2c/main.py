5cd73a1d3c87a2a808d71ebc8534c27b4b7c6849
Photontech <102208603+GitPhotontech@users.noreply.github.com>
2022-05-31 17:11:45 +0300

    Update main.py

--- Example_MicroPython/lcd16x2-i2c/main.py
+++ Example_MicroPython/lcd16x2-i2c/main.py
@@ -1,20 +1,30 @@
-from machine import I2C
-from lcd_api import LcdApi
-from i2c_lcd import I2cLcd
+""" 
+PHOTONTECH 
+  S-Core 16x2 LCD örneği
+  
+  S-Core üzerinde ki GP0 ve GP1. pine pine bağlı lcd yazılımda gönderilen değeri lcd ekranına yazdırıyor.
+   
+  Bu örnek kod micropython ile yazılarak PHOTONTECH tarafından kamuya sunulmuştur.
+  
+  https://github.com/GitPhotontech/Photontech_S-Core
+  
+"""
+
+from machine import I2C         #i2c kütüphanesi dahil edildi.
+from lcd_api import LcdApi      #lcd kütüphanesi dahil edildi.
+from i2c_lcd import I2cLcd      #lcd için i2c kütüphanesi dahil edildi.
 import time
 
-I2C_ADDR     = 0x27
-I2C_NUM_ROWS = 2
-I2C_NUM_COLS = 16
+I2C_ADDR     = 0x27             #i2c haberleşme portu belirtildi.
+I2C_NUM_ROWS = 2                #lcd satır sayısı belirtildi.
+I2C_NUM_COLS = 16               #lcd sütün sayısı belirtildi.
 
-i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
-lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)   #i2c konfigürasyon ayarları yapıldı.
+lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)             #tanımlanan değerler lcd değişkenine atıldı.
 
-while True:
-    lcd.putstr("Photontech Elektronik")
-    time.sleep(1)
-    lcd.clear() 
-    time.sleep(1)
+while True:                                                         #sonsuz döngü oluşturuldu.
+    lcd.putstr("Photontech Elektronik")                             #lcd ekranına yazı gönderildi.
+    time.sleep(1)                                                   #1 saniye bekleme.
+    lcd.clear()                                                     #lcd ekranı temizlendi.
+    time.sleep(1)                                                   #1 saniye bekleme.
 
-
-#lcd.putstr("Hello from\n"+chr(0)+" peppe8o.com "+chr(0))
